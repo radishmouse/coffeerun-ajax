1047d8d61de71570d256ebee73d9742c8f67e94e
Chris Aquino <chris.aquino@gmail.com>
2017-12-12 13:33:33 -0500

    uses static data

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,16 @@
-var SERVER_URL = 'http://dc-coffeerun.herokuapp.com/api/coffeeorders/';
+var SERVER_URL = '/data.json';
+/*
+
+Use the local data via python with:
+
+python -m SimpleHTTPServer
+
+or for python3:
+
+python -m http.server
+
+*/
+
 
 var SELECTORS = {
   'LIST': '[data-list]'
